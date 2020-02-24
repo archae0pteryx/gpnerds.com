b0b15a87a946fc34d6f6be3433f20fe431fc2ccd
archae0pteryx <thefakeyran@icloud.com>
2020-02-24 14:32:33 -0800

    WIP

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-export default function Header () {
-  return <div className='nav-container-absolue'>
-    <div className='nav-container'>
-      <a className='nav-link' href='https://github.com/archae0pteryx'>Github</a>
-      <a className='nav-link' href='mailto:root@gpnerds.com'>Contact</a>
+export default function Header() {
+  return (
+    <div className="nav-container-absolue">
+      <div className="nav-container">
+        <div className="nav-link-container animated fadeInUp">
+          <a className="nav-link" href="https://github.com/archae0pteryx">
+            Projects
+          </a>
+        </div>
+        <div className="nav-link-container animated fadeInUp delay-1s">
+          <a className="nav-link" href="https://linkedin.com/in/archae0pteryx">
+            LinkedIn
+          </a>
+        </div>
+        <div className="nav-link-container animated fadeInUp delay-2s">
+          <a className="nav-link" href="mailto:root@gpnerds.com">
+            Contact
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
+  )
 }
